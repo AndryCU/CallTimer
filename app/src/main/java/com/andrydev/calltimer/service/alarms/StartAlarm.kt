7a920c5fda24c796297a4cef344dea7ad5045909
AndryCU <74194123+AndryCU@users.noreply.github.com>
2022-04-27 18:04:25 -0400

    set vibrate mode when starts and normal mode when finished

--- app/src/main/java/com/andrydev/calltimer/service/alarms/StartAlarm.kt
+++ app/src/main/java/com/andrydev/calltimer/service/alarms/StartAlarm.kt
@@ -3,19 +3,20 @@ package com.andrydev.calltimer.service.alarms
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.os.Build
-import android.util.Log
 import com.andrydev.calltimer.service.ForegroundService
 
 class StartAlarm: BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent) {
+        val audiomanager= context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audiomanager.ringerMode=AudioManager.RINGER_MODE_VIBRATE
         val i= Intent(context, ForegroundService::class.java)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Log.d("ZXCV","onReceive BroadcastReceiver")
-            context?.startForegroundService(i)
+            context.startForegroundService(i)
         }
         else{
-            context?.startService(i)
+            context.startService(i)
         }
     }
 }
--- app/src/main/java/com/andrydev/calltimer/service/alarms/StopAlarm.kt
+++ app/src/main/java/com/andrydev/calltimer/service/alarms/StopAlarm.kt
@@ -3,12 +3,15 @@ package com.andrydev.calltimer.service.alarms
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import com.andrydev.calltimer.Preferences
 import com.andrydev.calltimer.service.ForegroundService
 
 class StopAlarm: BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        val audiomanager= context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audiomanager.ringerMode= AudioManager.RINGER_MODE_NORMAL
         val i=Intent(context,ForegroundService::class.java)
         val preferences=Preferences(context)
         preferences.saveAlarmStarted(false)
