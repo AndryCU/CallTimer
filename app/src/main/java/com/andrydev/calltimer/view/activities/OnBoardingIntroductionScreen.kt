82ff8f33582459a7ab290452c778d1959be48bc2
AndryCU <74194123+AndryCU@users.noreply.github.com>
2022-05-01 18:35:22 -0400

    1v introduction screen

---
+++ app/src/main/java/com/andrydev/calltimer/view/activities/OnBoardingIntroductionScreen.kt
@@ -0,0 +1,84 @@
+package com.andrydev.calltimer.view.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.viewpager.widget.ViewPager
+import com.andrydev.calltimer.Preferences
+import com.andrydev.calltimer.R
+import com.andrydev.calltimer.view.onboarding.OnBoardingData
+import com.andrydev.calltimer.view.onboarding.OnBoardingViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+
+class OnBoardingIntroductionScreen : AppCompatActivity() {
+    lateinit var preferences: Preferences
+    var onBoardingViewPagerAdapter: OnBoardingViewPagerAdapter?=null
+    var tabLayout: TabLayout?=null
+    var onBoardingViewPager: ViewPager? = null
+    var next: TextView?=null
+    var position=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        preferences= Preferences(this)
+        if (preferences.getsaveInit()){
+            val i= Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
+        setContentView(R.layout.activity_on_boarding_introduction_screen)
+        supportActionBar?.hide()
+        tabLayout=findViewById(R.id.tab_indicator)
+        next=findViewById(R.id.textsiguiente)
+        val onBoardingData: MutableList<OnBoardingData> = ArrayList()
+        onBoardingData.add(OnBoardingData("CallTimer","App diseñada para mantener el tono " +
+                "de tus llamadas en silencio o vibración en el período de tiempo que lo decidas.",R.raw.waiting))
+        onBoardingData.add(OnBoardingData("Trabajo","Podrás asistir a reuniones y " +
+                "trabajar con eficiencia sin molestas interrupciones.",R.raw.teamwork))
+        onBoardingData.add(OnBoardingData("Escuela","CallTimer te permitirá concentrarte en tus clases sin que el " +
+                "tono de tus llamadas afecte ni a ti, ni a tus compañeros.",R.raw.teacher))
+        onBoardingData.add(OnBoardingData("Familia y amigos","No te preocupes. No dejarás de recibir llamadas en ningún momento.",R.raw.carousel))
+
+        setOnBoardingViewPagerAdapter(onBoardingData)
+        position=onBoardingViewPager!!.currentItem
+        next?.setOnClickListener {
+            if (position<onBoardingData.size){
+                position++
+                onBoardingViewPager!!.currentItem=position
+            }
+            if (position==onBoardingData.size){
+                preferences.saveInit(true)
+                val i= Intent(this,MainActivity::class.java)
+                startActivity(i)
+            }
+        }
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                position=tab!!.position
+                if (tab.position==onBoardingData.size-1){
+                    next!!.text="Comencemos"
+                }
+                else{
+                    next!!.text="Siguiente"
+                }
+            }
+        })
+
+    }
+
+    private fun setOnBoardingViewPagerAdapter(onBoardingData: List<OnBoardingData>){
+        onBoardingViewPager=findViewById(R.id.screenPager)
+        onBoardingViewPagerAdapter= OnBoardingViewPagerAdapter(this,onBoardingData)
+        onBoardingViewPager!!.adapter=onBoardingViewPagerAdapter
+        tabLayout?.setupWithViewPager(onBoardingViewPager)
+    }
+}
---
+++ app/src/main/java/com/andrydev/calltimer/view/onboarding/OnBoardingData.kt
@@ -0,0 +1,3 @@
+package com.andrydev.calltimer.view.onboarding
+
+data class OnBoardingData(val tittle:String, val desc:String, val imageUrl:Int)
