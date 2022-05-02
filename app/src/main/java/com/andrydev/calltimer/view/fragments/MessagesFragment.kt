781885e6eb40a72de6d2d0836bb412dd226de68c
AndryCU <74194123+AndryCU@users.noreply.github.com>
2022-05-02 11:57:07 -0400

    ask and check SMS_SEND permission is granted or not

--- app/src/main/java/com/andrydev/calltimer/view/fragments/MessagesFragment.kt
+++ app/src/main/java/com/andrydev/calltimer/view/fragments/MessagesFragment.kt
@@ -1,22 +1,24 @@
 package com.andrydev.calltimer.view.fragments
 
+import android.Manifest.permission
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.andrydev.calltimer.adapters.MessagesAdapter
-
 import com.andrydev.calltimer.databinding.FragmentMessagesBinding
-import com.andrydev.calltimer.model.entities.MessageEntity
 import com.andrydev.calltimer.view.dialogs.DialogSMS
 import com.andrydev.calltimer.viewmodel.MessageViewModel
-import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MessagesFragment : Fragment() {
@@ -34,7 +36,7 @@ class MessagesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.fabAddMessage.setOnClickListener {
-            dialogSMS.show(parentFragmentManager,"CustomDialog")
+            activityResultLauncher.launch(permission.SEND_SMS)
         }
         messageViewModel.messages().observe(viewLifecycleOwner) {
             if (it.isNotEmpty()){
@@ -44,4 +46,29 @@ class MessagesFragment : Fragment() {
         }
     }
 
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){ isGranted ->
+            if (isGranted) {
+                dialogSMS.show(parentFragmentManager,"CustomDialog")
+            } else {
+                val showRationale = shouldShowRequestPermissionRationale(permission.SEND_SMS)
+                if (!showRationale){
+                    val b= AlertDialog.Builder(requireContext())
+                        .setTitle("Permiso no concedido")
+                    b.setMessage("Necesitamos este permiso para enviar de forma automática los SMS que configura." +
+                            " No leemos los SMS que tiene guardado ni enviamos ningún tipo de información.")
+                    b.setPositiveButton("Conceder"){_,_->
+                        val i= Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri= Uri.fromParts("package", activity?.packageName,null)
+                        i.data=uri
+                        startActivity(i)
+                    }
+                    b.setNegativeButton("Denegar"){_,_->
+                    }
+                    b.show()
+                }
+            }
+        }
 }
+
